9ef1e393935d7737bcf520dd0cc584df516ea6db
Th1f <irvinh145@gmail.com>
2025-03-08 18:12:49 +1100

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
-      </head>
+      <head></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
